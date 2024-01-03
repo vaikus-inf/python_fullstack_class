0ea01a6ba47fbf8e2936c437ee3088fd82bd0053
Ilya Poklitar <vaikus@mail.ru>
2024-01-03 20:52:18 +1100

    add in module_2 lesson_9 solution_2.py

--- module_2/lesson_9/src/solution_2.py
+++ module_2/lesson_9/src/solution_2.py
@@ -1,4 +1,7 @@
-def convert_to_hex(*args):
-    return(hex(args[0])[2:] + hex(args[1])[2:] + hex(args[2])[2:])
-
-print(convert_to_hex(255, 0, 1))
+def convert_to_hex(r: int = 0, g: int = 0, b: int = 0) -> str: 
+    return(f'#{hex(r)[-2:]}{hex(g)[-2:]}{hex(b)[-2:]}'.replace('x', '0').upper())
+    
+print(f'HEX: {convert_to_hex(255, 0, 0)}')
+print(f'HEX: {convert_to_hex(0, 255, 0)}')
+print(f'HEX: {convert_to_hex(0, 0, 255)}')
+print(f'HEX: {convert_to_hex(7, 36, 110)}')
