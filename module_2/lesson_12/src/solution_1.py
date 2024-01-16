3dabe273900645ce37bf781c100b445e77892e76
Ilya Poklitar <vaikus.gm@gmail.com>
2024-01-16 22:28:29 +1100

    edit module_2/lesson_12/src/solution_1.py

--- module_2/lesson_12/src/solution_1.py
+++ module_2/lesson_12/src/solution_1.py
@@ -1,11 +1,10 @@
+import statistics
+
 def collect_data(data: list[int]) -> None:
     def process_data(data: list[int]) -> None:
-        def summarize_data(average_value: float) -> None:
-            if average_value > int(average_value):
-                print(f'Итог: Среднее значение: {average_value}')
-            else:
-                print(f'Итог: Среднее значение: {int(average_value)}')
-        summarize_data(sum(data) / len(data))
+        def summarize_data(average_value) -> None:
+            print(f'Итог: Среднее значение: {average_value}')
+        summarize_data(statistics.mean(data))
     process_data(data)
 
 collect_data([1, 2, 3, 4, 5])
