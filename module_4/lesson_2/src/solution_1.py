b8db46e64a6a33dd39cb252a9a555271f48c67e9
Ilya Poklitar <vaikus.gm@gmail.com>
2024-04-04 22:53:31 +1100

    edit solution_1.py in module_4/lesson_2/src

---
+++ module_4/lesson_2/src/solution_1.py
@@ -0,0 +1,15 @@
+import json
+
+with open('module_4/lesson_2/src/sales.json') as file:
+    data = json.load(file)
+
+revenue_by_category: dict[str, int] = {}
+
+for item in data['sales']:
+    if item['category'] in revenue_by_category:
+        revenue_by_category[item['category']] += item['total_price']
+    else:
+        revenue_by_category[item['category']] = item['total_price']
+
+for key, value in revenue_by_category.items():
+    print(f'Выручка по категории {key}: {value}')
