e76d385779101edba50b51ac93d303be18ef9aab
vaikus-inf <vaikus.gm@gmail.com>
2024-04-05 17:01:06 +1100

    add solution_2.py in module_4/lesson_2/src

--- module_4/lesson_2/src/solution_1.py
+++ module_4/lesson_2/src/solution_1.py
@@ -1,6 +1,6 @@
 import json
 
-with open('module_4/lesson_2/src/sales.json') as file:
+with open('module_4/lesson_2/src/sales.json', encoding='utf-8') as file:
     data = json.load(file)
 
 revenue_by_category: dict[str, int] = {}
