a6deb6586955dfe3ae07658c47972e66b048f01b
vaikus-inf <vaikus.gm@gmail.com>
2024-04-09 17:18:46 +1100

    edit solution_3 in module_4/lesson_2/src

--- module_4/lesson_2/src/solution_3/solution_3.py
+++ module_4/lesson_2/src/solution_3/solution_3.py
@@ -9,20 +9,25 @@ class Client:
         self.bonuses = bonuses
 
     @staticmethod
-    def data_verification(name: str, surname: str, birthday: str, bonuses: str) -> bool:
+    def name_verification(name: str, surname: str) -> bool:
         if not re.match(r'^[а-яА-ЯёЁ]+$', name) or not re.match(r'^[а-яА-ЯёЁ]+$', surname):
             return False
-        
+        return True
+    
+    @staticmethod
+    def birthday_verification(birthday: str) -> bool:    
+        birthday_date_type: date = datetime.strptime(birthday, "%d.%m.%Y").date()
+        if not (date(1950, 1, 1) <= birthday_date_type <= date.today()):
+            return False
+        return True
+    
+    @staticmethod
+    def bonuses_verification(bonuses: str) -> bool:    
         try:
             if not (0 <= int(bonuses) <= 10000000):
                 return False
         except ValueError:
             return False
-        
-        birthday_date_type: date = datetime.strptime(birthday, "%d.%m.%Y").date()
-        if not (date(1950, 1, 1) <= birthday_date_type <= date.today()):
-            return False
-        
         return True
 
 processed_clients: int = 0
@@ -33,7 +38,7 @@ with open('module_4/lesson_2/src/solution_3/clients.csv', 'r', encoding='utf-8')
     reader = csv.reader(csv_file)
     for i, row in enumerate(reader):
         if i > 0:
-            if Client.data_verification(row[0], row[1], row[2], row[3]):
+            if Client.name_verification(row[0], row[1]) and Client.birthday_verification(row[2]) and Client.bonuses_verification(row[3]):
                 clients_dict['clients'].append({
                     'name': row[0],
                     'surname': row[1],
